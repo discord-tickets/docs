ce41f1cae3d17b6141fc3dbb16762ffcbf8dd1f9
Isaac <git@eartharoid.me>
2022-10-02 21:53:20 +0100

    fix feedback with instant navigation?

--- overrides/javascripts/feedback.js
+++ overrides/javascripts/feedback.js
@@ -1,27 +1,36 @@
 const umamiSite = "{{ config.extra.analytics.website }}";
-const feedback = document.forms.feedback;
-if (feedback) {
-	/* Show feedback */
-	feedback.hidden = false;
 
-	feedback.addEventListener("submit", function (ev) {
-		ev.preventDefault();
+function feedback () {
+	const form = document.forms.feedback;
+	if (form) {
+		/* Show feedback */
+		form.hidden = false;
+		form.outerHTML = form.outerHTML;
+		form.addEventListener("submit", function (ev) {
+			ev.preventDefault();
 
-		/* Retrieve page and feedback value */
-		let page = document.location.pathname;
-		let data = ev.submitter.getAttribute("data-md-value");
+			/* Retrieve page and feedback value */
+			let page = document.location.pathname;
+			let data = ev.submitter.getAttribute("data-md-value");
 
-		/* Disable form and show note, if given */
-		feedback.firstElementChild.disabled = true;
-		let note = feedback.querySelector(
-			".md-feedback__note [data-md-value='" + data + "']"
-		);
-		if (note) note.hidden = false;
+			/* Disable form and show note, if given */
+			form.firstElementChild.disabled = true;
+			let note = form.querySelector(
+				".md-feedback__note [data-md-value='" + data + "']"
+			);
+			if (note) note.hidden = false;
 
-		/* Send feedback value */
-		console.log(page, data);
-		umami.trackEvent('feedback', { value: data }, page, umamiSite);
-	});
+			/* Send feedback value */
+			console.log(page, data);
+			umami.trackEvent('feedback', { value: data }, page, umamiSite);
+		});
+	}
 }
+
+feedback();
+
+document.addEventListener("DOMContentLoaded", function () {
+	feedback();
+});
 
 
