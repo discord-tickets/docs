23b48c745a4228dc189f711d05314e56653812e2
Isaac <git@eartharoid.me>
2022-10-04 00:52:42 +0100

    number formatting

--- overrides/javascripts/home.js
+++ overrides/javascripts/home.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", async function () {
 	const data = await (await fetch('https://stats.discordtickets.app/api/v3/current')).json();
-	document.getElementById('ea-stats-users').innerHTML = data.activated_users;
-	document.getElementById('ea-stats-tickets').innerHTML = data.tickets;
-	document.getElementById('ea-stats-servers').innerHTML = data.guilds.total;
+	document.getElementById('ea-stats-users').innerHTML = data.activated_users.toLocaleString(navigator.languages[0]);
+	document.getElementById('ea-stats-tickets').innerHTML = data.tickets.toLocaleString(navigator.languages[0]);
+	document.getElementById('ea-stats-servers').innerHTML = data.guilds.total.toLocaleString(navigator.languages[0]);
 });
