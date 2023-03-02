bb66de71f922deb1bde92afe982f0132d6fcfde2
Isaac <git@eartharoid.me>
2023-03-02 21:15:24 +0000

    Update config

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-apt-get install libcairo2-dev libfreetype6-dev libffi-dev libjpeg-dev libpng-dev libz-dev
+apt-get install libcairo2-dev libfreetype6-dev libffi-dev libjpeg-dev libpng-dev libz-dev pngquant
 pip install -r requirements.txt
 pip install pillow cairosvg wheel git+https://${GH_TOKEN}@github.com/eartharoid/mkdocs-material-insiders.git
 mkdocs build --config-file mkdocs.insiders.yml
